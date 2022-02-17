a1643d25600c06325c251b63c3fbfc0eeec5e647
21P31A0436 <99795947+21P31A0436@users.noreply.github.com>
2022-02-17 15:05:07 +0530

    #include<stdio.h> int main() {     int a, b, c;    // printf("enter a value");     scanf("%d ",&a);    // printf("enter b value");     scanf("%d",&b);     c=a*b;     printf("%d",c);     return 0 ; }

---
+++ Multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() 
+{
+    int a, b, c;
+   // printf("enter a value");
+    scanf("%d ",&a);
+   // printf("enter b value");
+    scanf("%d",&b);
+    c=a*b;
+    printf("%d",c);
+    return 0 ;
+}
