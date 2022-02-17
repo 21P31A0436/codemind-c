737b0ca3958a4f771985e0de8b84167f93ab7afa
21P31A0436 <99795947+21P31A0436@users.noreply.github.com>
2022-02-17 15:12:02 +0530

    #include<stdio.h> int main() {     int n,sqr, rem, sum=0;     scanf("%d",&n);     sqr=n*n;     while(sqr!=0)     {         rem=sqr%10;         sum=sum+rem;         sqr=sqr/10;     }     if(sum==n)     printf("Neon Number");     else     printf("Not Neon Number");     return 0; }

---
+++ Neon_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+    int n,sqr, rem, sum=0;
+    scanf("%d",&n);
+    sqr=n*n;
+    while(sqr!=0)
+    {
+        rem=sqr%10;
+        sum=sum+rem;
+        sqr=sqr/10;
+    }
+    if(sum==n)
+    printf("Neon Number");
+    else
+    printf("Not Neon Number");
+    return 0;
+}
