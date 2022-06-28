ea7756b06d6d60e7006a8271cf36ca54d46ec1a9
21P31A0436 <99795947+21P31A0436@users.noreply.github.com>
2022-06-28 23:45:11 +0530

    #include<stdio.h> int main() {     int arr[100],n,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]!=0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]==0)         {             printf("%d ",arr[i]);         }     }
    }

---
+++ Move_Zeroes.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    
+}
